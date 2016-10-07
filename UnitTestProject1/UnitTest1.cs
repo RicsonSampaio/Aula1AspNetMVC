d20f01285a19f823f8666442cdb8968304d11c4e
Ricson Herculano Sampaio <Ricson Herculano Sampaio>
2016-10-07 13:48:34 -0300

    Adição projeto de testes

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Withdraw_ValidAmount_ChangesBalance()
+        {
+            // arrange
+            double currentBalance = 10.0;
+            double withdrawal = 1.0;
+            //double expected = 9.0;
+            var account = currentBalance + withdrawal;
+            // act
+            //account.Withdraw(withdrawal);
+            //double actual = account.Balance;
+            // assert
+            Assert.AreEqual(account, 18.0);
+        }
+    }
+}
